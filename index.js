e949b4e010661b5c0647a55f5ad361189eb58865
Artemiy <andrikv@gmail.com>
2022-10-07 00:13:45 +0300

    Fixed route companiesReady

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const app = express()
 app.use(express.json())
 
 // Роуты
-app.get('/', (req, res) => {
+app.get('/companiesReady', (req, res) => {
 	res.json(companiesReady)
 })
 
 // Работа сервера
 app.listen(process.env.port || port, (err) => {
 	if (err) console.log(`[ERROR] ${err.name}`)
-	else console.log(`[STATUS] Server started on port: ${port}`)
+	else console.log(`[STATUS] Server started at port: ${port}`)
 })
