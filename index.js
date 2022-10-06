3bbd435bb7f689be193a346e72def590295df7e6
Artemiy <andrikv@gmail.com>
2022-10-07 00:05:04 +0300

    server configured

--- index.js
+++ index.js
@@ -1,44 +1,22 @@
 // Библиотеки
 import express from 'express'
-import fs from 'fs'
 
-// Получение data
-import dataCompany from './data/dataCompanyDirectory.json' assert { type: 'json' }
-import dataPoll from './data/dataPollResults.json' assert { type: 'json' }
-import dataRevenue from './data/dataRevenue.json' assert { type: 'json' }
+// Чистая дата
+import { companiesReady } from './utils/countingCash.js'
 
-// сумма для каждой компании по честному
-const companies = []
+// Настройка сервера
+const port = '3000'
+const app = express()
 
-dataCompany['Справочник компаний'].forEach((company) => {
-	companies.push({
-		id: company.id,
-		name: company['Наименование'],
-		industry: company['Отрасль'],
-		pollCash: 0,
-		revenueCash: 0,
-	})
-})
-console.log(companies)
-dataPoll['Результаты опроса о доходах'].forEach((item, i) => {
-	const thisCompany = companies.filter((company) => company.name === item['Компания'])[0]
-	console.log(item['Компания'])
-	console.log(thisCompany.pollCash)
-	thisCompany.pollCash += i
+app.use(express.json())
+
+// Роуты
+app.get('/', (req, res) => {
+	res.json(companiesReady)
 })
 
-console.log(companies)
-
-// const port = '3000'
-// const app = express()
-
-// app.use(express.json())
-
-// app.get('/', (req, res) => {
-// 	res.json('Yoo!')
-// })
-
-// app.listen(process.env.port || port, (err) => {
-// 	if (err) console.log(`[ERROR] ${err.name}`)
-// 	else console.log(`[STATUS] Server started on port: ${port}`)
-// })
+// Работа сервера
+app.listen(process.env.port || port, (err) => {
+	if (err) console.log(`[ERROR] ${err.name}`)
+	else console.log(`[STATUS] Server started on port: ${port}`)
+})
