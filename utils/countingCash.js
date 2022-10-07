779dd1d58c76ce9793b453c139d85b62dfd00f2e
Artemiy <andrikv@gmail.com>
2022-10-08 00:05:36 +0300

    Fixed problem with min, max and avg

--- utils/countingCash.js
+++ utils/countingCash.js
@@ -1,34 +1,34 @@
 // Получение очищенной data
 import companies from './../datasets/companies.js'
 import pollResults from './../datasets/pollResults.js'
-import revenueResults from './../datasets/revenueResults.js'
-
+import usersCashStats from './../datasets/revenueResults.js'
 import { median } from './/median.js'
 
-// Подсчет суммы для результатов по опросам
+// Подсчет суммы для результатов по опросам (неоф)
 pollResults.forEach((item) => {
 	const thisCompany = companies.find((cmp) => cmp.name === item.company)
 
 	thisCompany.pollCashValues.push(item.cash)
 	thisCompany.totalPollCash += item.cash
-	thisCompany.avgPollCash = thisCompany.totalPollCash / pollResults.length
+	thisCompany.avgPollCash = thisCompany.totalPollCash / thisCompany.pollCashValues.length
 	thisCompany.medianPollCash = median(thisCompany.pollCashValues)
 	thisCompany.minPollCash = Math.min(item.cash, thisCompany.minPollCash)
 	thisCompany.maxPollCash = Math.max(item.cash, thisCompany.maxPollCash)
 })
 
-// Подсчет суммы для результатов по инф. о доходах
-revenueResults.forEach((item) => {
-	const thisCompany = companies.find((cmp) => cmp.id === item.companyId)
+// Подсчет суммы для результатов по статистике (оф)
+for (const [_, values] of Object.entries(usersCashStats)) {
+	const thisCompany = companies.find((cmp) => cmp.id === values.companyId)
 
-	thisCompany.revenueCashValues.push(item.cash)
-	thisCompany.totalRevenueCash += item.cash
-	thisCompany.avgRevenueCash = thisCompany.totalRevenueCash / revenueResults.length
+	thisCompany.revenueCashValues.push(values.cash)
+	thisCompany.totalRevenueCash += values.cash
+	thisCompany.avgRevenueCash = thisCompany.totalRevenueCash / thisCompany.revenueCashValues.length
 	thisCompany.medianRevenueCash = median(thisCompany.revenueCashValues)
-	thisCompany.minRevenueCash = Math.min(item.cash, thisCompany.maxRevenueCash)
-	thisCompany.maxRevenueCash = Math.max(item.cash, thisCompany.maxRevenueCash)
-})
+	thisCompany.minRevenueCash = Math.min(values.cash, thisCompany.maxRevenueCash)
+	thisCompany.maxRevenueCash = Math.max(values.cash, thisCompany.maxRevenueCash)
+}
 
+// Удаление ненужных значений из итоговой даты
 companies.forEach((company) => {
 	company.pollCashValues = []
 	company.revenueCashValues = []
