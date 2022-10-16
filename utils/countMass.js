a90e35ffa34dc441a24e93a1be29d6d68604e438
Artemiy <andrikv@gmail.com>
2022-10-16 16:55:13 +0300

    fixed func for countMass by formule

--- utils/countMass.js
+++ utils/countMass.js
@@ -1,13 +1,12 @@
 const countMass = (P, T, Pkr, Tkr, R) => {
 	const v = 0.1
 	const Pavg = P / Pkr
-	const Tavg = (T / Tkr) ^ -3.668
-	const Z = 1 - 0.4273 * Pavg * Tavg
+	const Tavg = T / Tkr
+	const Z = 1 - 0.4273 * Pavg * Math.pow(Tavg, -3.668)
 	const Ro = P / (Z * R * T)
 	const m = v * Ro
 
 	return m
-	//return (0.1 * P) / (1 - 0.4723 * (P / Pkr) * ((T / Tkr) ^ -3.668) * R * T)
 }
 
 export default countMass
